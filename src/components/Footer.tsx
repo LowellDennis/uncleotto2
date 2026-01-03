2277f45e92564656918e69af5bb475de758dc429
Lowell Dennis <Lowell.Dennis@hpe.com>
2026-01-03 03:06:45 -0600

    Update version to 2026.0103.0300

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import './Footer.css'
 
-const APP_VERSION = '2026.0102.1700' // YYYY.MMDD.HHmm format
+const APP_VERSION = '2026.0103.0300' // YYYY.MMDD.HHmm format
 
 export function Footer() {
   return (
